55d4b411a79559abd82d4e795dca78f4c9fd2bd7
profproix <158355109+profproix@users.noreply.github.com>
2025-10-15 20:11:37 -0700

    Update higherOrderFunctions.js

--- Week3/starter/higherOrderFunctions.js
+++ Week3/starter/higherOrderFunctions.js
@@ -10,3 +10,16 @@ const books = [
   "Dungeon Crawler Carl",
   "The Southern Reach Trilogy",
 ];
+
+const booksAndPrices = [
+  { title: "The Broken Earth Trilogy", price: 30 },
+  { title: "The Raven Tower", price: 29 },
+  { title: "Ancillary Justice", price: 15 },
+  { title: "Babel", price: 20 },
+  { title: "Moon of the Crusted Snow", price: 15 },
+  { title: "The Poppy War", price: 24 },
+  { title: "What Moves the Dead", price: 12 },
+  { title: "Priory of the Orange Tree", price: 45 },
+  { title: "Dungeon Crawler Carl", price: 30 },
+  { title: "The Southern Reach Trilogy", price: 25 },
+];
